b942fb26a7dfdb3b70a61a28efa26920be6cba66
John Wesley <112788953+JohnWesley14@users.noreply.github.com>
2026-06-10 15:36:24 -0300

    Update atv1.php

--- atv1.php
+++ atv1.php
@@ -29,7 +29,7 @@ verificarNumero(6);
 --------------------------- Processo e Lógica ---------------------------------
 
 1. Criei um if que verifica se o número faz parte do conjunto de 1 a 31, senão, exibe mensagem de erro, se estiver segue para o passo 2.
-2. Criei um array com os 31 valores e verifico por meio da função in_array se o número selecionado está na função. (Como já foi feita a verificação anteriormente esse if sempre resultará positivo, mas por conta do desafio pedir essa verificação, assim o fiz).
+2. Criei um array com os 31 valores e verifico por meio da função in_array se o número selecionado está na função. 
 3. Utilizei o método foreach para percorrer o array e ver onde o número selecionado coincide com o valor, após isso, capturo o índice e exibo, como o output do problema pede.
 
 --------------------------- Observações ----------------------------
